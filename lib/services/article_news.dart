96ff0a253e3a9c33d83f31011d7e5e9ee5456a62
AfilAhamed <afilahamed0007@gmail.com>
2023-11-09 13:39:53 +0530

    dio added

--- lib/services/article_news.dart
+++ lib/services/article_news.dart
@@ -1,19 +1,18 @@
-import 'dart:convert';
-
+import 'package:dio/dio.dart';
 import 'package:news_api/model/article_model.dart/articlemodel.dart';
-import 'package:http/http.dart' as http;
 
 class ArticleNewsServices {
   List<ArticleModel> news = [];
 
   Future<void> fetchArticleNews() async {
+    final Dio dio = Dio();
     String url =
         'https://newsapi.org/v2/top-headlines?country=in&apiKey=9a922478bde34c1298d3aba0b7a4f66d';
 
-    var response = await http.get(Uri.parse(url));
+    var response = await dio.get(url);
 
     if (response.statusCode == 200) {
-      var jsonData = jsonDecode(response.body);
+      var jsonData = response.data;
 
       jsonData['articles'].forEach((element) {
         if (element['urlToImage'] != null && element['description'] != null) {
--- lib/services/category_new.dart
+++ lib/services/category_new.dart
@@ -1,18 +1,20 @@
-import 'dart:convert';
+// import 'dart:convert';
+import 'package:dio/dio.dart';
 import 'package:news_api/model/article_model.dart/articlemodel.dart';
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 
 class CategoryNewsServices {
   List<ArticleModel> news = [];
 
   Future<void> fetchCategoryNews(String category) async {
+    final Dio dio = Dio();
     String url =
         'https://newsapi.org/v2/top-headlines?country=in&category=$category&apiKey=9a922478bde34c1298d3aba0b7a4f66d';
 
-    var response = await http.get(Uri.parse(url));
+    var response = await dio.get(url);
 
     if (response.statusCode == 200) {
-      var jsonData = jsonDecode(response.body);
+      var jsonData = response.data;
       jsonData['articles'].forEach((element) {
         if (element['urlToImage'] != null && element['description'] != null) {
           ArticleModel articleModel = ArticleModel(
